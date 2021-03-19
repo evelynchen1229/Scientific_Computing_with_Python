5ec9dbd94bf11464656f4b895a5be3919fb7d271
Evelyn Chen <32022542+evelynchen1229@users.noreply.github.com>
2021-03-19 12:21:16 +0000

    Update probability_calculator.py

--- probability_calculator/probability_calculator.py
+++ probability_calculator/probability_calculator.py
@@ -13,7 +13,6 @@ class Hat():
             self.key = key
             self.value = value
             content += [self.key] * self.value
-     #   print(content)
         self.content = content
 
     def draw(self, num_balls):
@@ -24,49 +23,24 @@ class Hat():
 
         while num_balls > 0:
             n = random.randint(0,(len(self.content)-1))
-            #print(n)
             balls.append(self.content.pop(n))
-            #print(balls)
             num_balls -= 1
-        #    print(num_balls)
-
         self.content += balls
         return balls
-
-
-
-
-
-# print(hat1.draw(4))
-#ball = hat1.draw(4)
-#print(ball)
-#print(ball.count('red'))
 
 def experiment(hat, expected_balls, num_balls, num_experiment):
     expected = 0
     num_exp = num_experiment
-#    print(num_exp)
     while num_exp:
- #       print(hat)
         result = hat.draw(num_balls)
-  #      print(result)
         l = []
         for k,v in expected_balls.items():
-   #         print(k,v)
             l.append(v <= result.count(k))
-    #        print(l)
-        #for i in range (0,len(expected_balls)):
-         #   ball = expected_balls.keys()
-          #  num = expected_balls.values()
-           # print(ball,num)
-            #l.append(
-            #print(expected_balls[expected_balls[i]] >= result.count(expected[i]))
 
         if sum(l) == len(l):
             expected += 1
         num_exp -= 1
-     #   print(expected,num_exp)
-
+        
     return round(expected/num_experiment,2)
 
 hat1 = Hat(yellow=1,red=3,blue=3)
